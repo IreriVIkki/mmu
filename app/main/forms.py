80222bf97971be7449a7ec684014cc3cd9d6a713
IreriVikki <wambsviki@gmail.com>
2018-10-03 10:46:01 +0300

    used form validatiors to make sure no input is left empty

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,12 @@
 
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class CalculatorForm(FlaskForm):
-    num1 = IntegerField('Enter First Number')
-    num2 = IntegerField('Enter Second Number')
+    num1 = IntegerField('Enter First Number', validators=[DataRequired()])
+    num2 = IntegerField('Enter Second Number', validators=[DataRequired()])
     operator = SelectField('Operator',
                            choices=[('Multiply', 'Multiply: x'), ('Add', 'Add: +'), ('Divide',
                                                                                      'Divide: /'), ('Subtract', 'Subtract: -'), ('Reminder', 'Reminder: %')]
